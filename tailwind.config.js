85ab8e237ff83892a0cf8c7c56e2a0c79a05a4e0
Muhammad Ikhsan Maulana <ikhsan118@gmail.com>
2022-04-20 13:24:16 +0700

    portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    container:{
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary:'#14b8a6',
+        dark:'#0f172a',
+        secondary:'#64748b'
+      },
+      screens: {
+        '2xl': '1320px',
+      }
+    },
+  },
+  plugins: [],
+}
