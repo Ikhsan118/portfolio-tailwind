98c464646da47882765fad3edc31d3aab6b1f5a8
Muhammad Ikhsan Maulana <ikhsan118@gmail.com>
2022-04-21 07:55:09 +0700

    update darkmode + perbaikan fitur

--- public/js/script.js
+++ public/js/script.js
@@ -4,11 +4,16 @@
 window.onscroll = function() {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
     if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 };
 
@@ -20,3 +25,39 @@ hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+//klik diluar hamburger menu
+document.addEventListener('click', function(e) {
+    if (!hamburger.contains(e.target)) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+}
+);
+
+
+//darkmode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function() {
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+        localStorage.theme='dark';
+    }
+    else {
+        html.classList.remove('dark');
+        localStorage.theme='light';
+    }
+});
+
+//pindahkan posisi toggle sesuai theme
+if (localStorage.theme === 'dark') {
+    darkToggle.checked = true;
+    html.classList.add('dark');
+}
+else {
+    darkToggle.checked = false;
+    html.classList.remove('dark');
+}
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   content: ['./public/**/*.{html,js}'],
+  darkMode: 'class',
   theme: {
     container:{
       center: true,
